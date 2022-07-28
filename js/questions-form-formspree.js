95394884c1b814c3cd890372e34b02407e4bd8cf
yurfrant <yur.frant@gmail.com>
2022-07-28 21:21:13 +0000

    added formspree integration to questions block

---
+++ js/questions-form-formspree.js
@@ -0,0 +1,31 @@
+(function () {
+    const form = document.querySelector(".main__questions-form");
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const status = document.querySelector(".questions-form-status");
+        fetch("https://formspree.io/f/xlevrrop", {
+            method: "POST",
+            body: new FormData(form),
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                status.innerHTML = "Thanks for your submission!";
+                form.reset();
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                    } else {
+                        status.innerHTML = "Oops! There was a problem submitting your form"
+                    }
+                })
+            }
+        }).catch(error => {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+        });
+    }
+    document.querySelector('.button-submit').addEventListener("click", handleSubmit);
+})();
