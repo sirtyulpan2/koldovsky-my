397a920af23157fd8731204e98a2a1467eafdca1
S8bik <katryach27@gmail.com>
2022-07-29 15:36:12 +0000

    menu

--- js/menu-nav.js
+++ js/menu-nav.js
@@ -1,16 +1,16 @@
-// document.addEventListener("DOMContentLoaded", function() {    
-//     const menuButton = document.querySelector('.menu-button');
-//     const menuClose = document.querySelector('.menu-close');
-//     const menu = document.querySelector('.header-navigation');
+document.addEventListener("DOMContentLoaded", function() {    
+    const menuButton = document.querySelector('.menu-button');
+    const menuClose = document.querySelector('.menu-close');
+    const menu = document.querySelector('.header__navigation');
 
-//     function openMenu(){
-//         menu.classList.add('active');
-//     }
+    function openMenu(){
+        menu.classList.add('active');
+    }
 
-//     function closeMenu(){
-//         menu.classList.remove('active');
-//     }
+    function closeMenu(){
+        menu.classList.remove('active');
+    }
 
-//     menuButton.addEventListener('click', openMenu);
-//     menuClose.addEventListener('click', closeMenu);
-// });
+    menuButton.addEventListener('click', openMenu);
+    menuClose.addEventListener('click', closeMenu);
+});
