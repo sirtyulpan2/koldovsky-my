ed77673c13ce11ec0a24e0036bedb4fc5fde68d0
S8bik <katryach27@gmail.com>
2022-07-29 13:05:01 +0000

    problems with menu

---
+++ js/menu-nav.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function() {    
+//     const menuButton = document.querySelector('.menu-button');
+//     const menuClose = document.querySelector('.menu-close');
+//     const menu = document.querySelector('.header-navigation');
+
+//     function openMenu(){
+//         menu.classList.add('active');
+//     }
+
+//     function closeMenu(){
+//         menu.classList.remove('active');
+//     }
+
+//     menuButton.addEventListener('click', openMenu);
+//     menuClose.addEventListener('click', closeMenu);
+// });
