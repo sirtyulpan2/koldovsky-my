715faad82e76fa108353732d061ef9424b72881d
YuraKhmil <yurakhmil66@gmail.com>
2022-08-01 13:51:36 +0300

    Change clients pets carousel

--- js/carousel_our_pets.js
+++ js/carousel_our_pets.js
@@ -2,13 +2,41 @@
 
     const slides = [
         `<div class="our_pets__slide"> 
-            <img src="img/jokie.webp" alt="Jokie">
+          <div class="about__pets">
+            <img class="my__pets" src="img/jokie.webp" alt="Jokie">
+            <h3 class="pets__name">Jokie</h3>
+            <div class="divider"></div>
+            <p class="pets__info">“My parents were worried about my health as I didn't want to eat at all. A vet
+            at
+            Best.Pet helped them a lot, explained the peculiarities of parrots feeding, and offered a
+            special
+            meal plan. Yummy!”</p>
+            <p class="pets__date">October 28, 2019</p>
+          </div>
         </div>`,
         `<div class="our_pets__slide"> 
-            <img src="img/axel.webp" alt="Axel">
+          <div class="about__pets">
+            <img class="my__pets" src="img/axel.webp" alt="Axel">
+            <h3 class="pets__name">Axel</h3>
+            <div class="divider"></div>
+            <p class="pets__info">“When Mummy traveled abroad, she couldn't take me with her. So, she called
+            Best.Pet, and Alina came to care for me for 2 weeks. I really liked Alina; we had so much fun
+            and
+            enjoyed long walks."</p>
+            <p class="pets__date">March 03, 2020</p>
+          </div>
         </div>`,
         `<div class="our_pets__slide"> 
-            <img src="img/milo.webp" alt="Milo">
+          <div class="about__pets">
+            <img class="my__pets" src="img/milo.webp" alt="Milo">
+            <h3 class="pets__name">Milo</h3>
+            <div class="divider"></div>
+            <p class="pets__info">"I have to take medications regularly. When my parents decided to go on a
+            vacation, they were worried about my feeding. They called a pet sitter Mary, and we liked each
+            other
+            so much!"</p>
+            <p class="pets__date">December 12, 2021</p>
+          </div>
         </div>`,
     ];
 
@@ -37,7 +65,7 @@
         renderCarousel();
     }
 
-    setInterval(next, 3000);
+    setInterval(next, 4000);
 
     const prevButton = document.querySelector('.our_pets-carousel__btn-prev');
     prevButton.addEventListener('click', prev);
