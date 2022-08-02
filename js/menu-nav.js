8efe35315b38f712d1c9239965b2e36ed03a22c3
S8bik <katryach27@gmail.com>
2022-08-02 15:39:59 +0000

    burger menu

--- js/menu-nav.js
+++ js/menu-nav.js
@@ -1,16 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {    
-    const menuButton = document.querySelector('.menu-button');
+// document.addEventListener("DOMContentLoaded", function() {    
+    const menuBurger = document.querySelector('.menu-burger');
     const menuClose = document.querySelector('.menu-close');
     const menu = document.querySelector('.header__navigation');
+    const menuLogo = document.querySelector('.header__container-logo');
 
     function openMenu(){
         menu.classList.add('active');
+        menuLogo.classList.add('active');
     }
 
     function closeMenu(){
         menu.classList.remove('active');
+        menuLogo.classList.remove('active');
     }
 
-    menuButton.addEventListener('click', openMenu);
+    menuBurger.addEventListener('click', openMenu);
     menuClose.addEventListener('click', closeMenu);
-});
+// });
